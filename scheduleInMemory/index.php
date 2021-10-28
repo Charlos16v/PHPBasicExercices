afcbef066b5f73fa8de557a38f22455c689c44c4
Carlos Uriel Domínguez Ruiz-Díaz <cdominguez@cifpfbmoll.eu>
2021-10-28 14:29:53 +0200

    feat: schedule in memory with array without saving hidden data in form

--- scheduleInMemory/index.php
+++ scheduleInMemory/index.php
@@ -1,22 +1,15 @@
 <!doctype html>
-<!-- Desarrollo Web en Entorno Servidor -->
-<!-- Tema 2 : Características del Lenguaje PHP -->
-<!-- Solución a la tarea: 
-    Creación de una agenda de contactos 
-    No utiliza bases de datos, ni sesiones. Almacena los contactos
-    dentro del formulario como campos oculos 
--->
 <html>
     <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <title>Agenda de contactos</title>
+        <title>Agenda</title>
     </head>
     <body>
         <?php
         if (isset($_GET['agenda']))
             $agenda = $_GET['agenda'];
         else
-            $agenda = array(); // Creamos $agenda como un array vacío  
+            $agenda = array();
         
         
         if (isset($_GET['submit']))
@@ -25,7 +18,7 @@
             $nuevo_telefono = filter_input(INPUT_GET,'telefono');
             if (empty($nuevo_nombre))
             {
-                echo "<p style='color:red'>Debe introducir un nombre!!</p><br />";
+                echo "<p style='color:red'>¡Se debe introducir un nombre!</p><br />";
             }
             elseif (empty($nuevo_telefono))
             {
@@ -38,26 +31,17 @@
         }
         ?>
 
-        <!-- Creamos el formulario de introducción de un nuevo contacto -->
         <h2>Nuevo contacto</h2>
 
         <form>
-            <!-- Metemos los contactos ya existentes ocultos en el formulario -->
             <div style="align-items: left">
-                <?php
-                foreach ($agenda as $nom => $telf) {
-                    echo '<input type="hidden" name="agenda[' . $nom . ']" ';
-                    echo 'value="' . $telf . '"/>';
-                }
-                ?>
                 <label>Nombre:<input type="text" name="nombre"/></label><br />
                 <label>Teléfono:<input type="number" name="telefono"/></label><br />
-                <input type="submit" name='submit' value="Ejecutar"/><br />
+                <input type="submit" name='submit' value="Guardar"/><br />
             </div>
         </form>
         <br />
 
-        <!-- Mostramos los contactos de la agenda -->
         <h2>Agenda</h2>
         <?php
         if (count($agenda) == 0)
@@ -67,8 +51,8 @@
         else
         {
             echo "<ul>";
-            foreach ($agenda as $nom => $telf) {
-                echo "<li>" . $nom . ': ' . $telf . "</li>";
+            foreach ($agenda as $nombre => $telefono) {
+                echo "<li>" . $nombre . ': ' . $telefono . "</li>";
             }
             echo "</ul>";
         }
